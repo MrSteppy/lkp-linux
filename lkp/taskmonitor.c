642b5a027050fda264a0520f9f24644d97bdaf28
Jonathan Remarque <jonathan.remarque@rwth-aachen.de>
2026-06-26 10:17:17 +0200

    Refactor taskmonitor module to use task_sample and task_monitor structs

--- lkp/taskmonitor.c
+++ lkp/taskmonitor.c
@@ -14,87 +14,74 @@ MODULE_DESCRIPTION("You can run, but you can't hide");
 MODULE_AUTHOR("Me");
 MODULE_LICENSE("GPL");
 
-struct task_stats {
-  pid_t pid;
+struct task_sample {
   u64 utime;
   u64 stime;
 };
 
-int task_stats_update_from_task_struct(struct task_stats *stats, const struct task_struct *task)
-{
-  if (!pid_alive(task)) {
-    pr_err("Process with pid %d is no longer alive\n", stats->pid);
-    return -1;
-  }
+struct task_monitor {
+  pid_t pid;
+};
 
-  stats->utime = task->utime;
-  stats->stime = task->stime;
-
-  return 0;
-}
-
-int task_stats_update_from_pid(struct task_stats *stats, struct pid *pid)
-{
-  struct task_struct *task = get_pid_task(pid, PIDTYPE_PID);
-
-  if (!task) {
-    pr_err("Cannot query task information for process with pid %d\n", stats->pid);
-    return -1;
-  }
-
-  int err = task_stats_update_from_task_struct(stats, task);
-
-  put_task_struct(task);
-
-  if (err) {
-    return err - 1;
-  }
-
-  return 0;
-}
-
-int task_stats_update(struct task_stats *stats)
-{
-  pid_t pid_nr = stats->pid;
-  struct pid *pid = find_get_pid(pid_nr);
-
-  if (!pid) {
-    pr_err("Cannot fid a process with pid %d\n", pid_nr);
-    return -1;
-  }
-
-  int err = task_stats_update_from_pid(stats, pid);
-
-  put_pid(pid);
-
-  if (err) {
-    return err - 1;
-  }
-
-  return 0;
-}
-
-int task_stats_render(const struct task_stats *stats, char *buf, size_t buffer_len)
-{
-  return snprintf(buf, buffer_len, "pid %d usr %llu sys %llu", stats->pid, stats->utime, stats->stime);
-}
-
-static int target;
+static pid_t target;
 module_param(target, int, 0444);
 MODULE_PARM_DESC(target, "PID of the process to monitor");
 
-static struct task_stats stats;
+static struct task_sample task_sample;
+static struct task_monitor task_monitor;
 static struct task_struct *monitor_fn_handle;
 
 static unsigned int major;
 static struct file_operations fops;
 
+static bool get_sample(const struct task_monitor *monitor, struct task_sample *sample)
+{
+  bool sample_okay = false;
+  pid_t pid_nr = monitor->pid;
+  struct pid *pid = find_get_pid(pid_nr);
+
+  if (!pid) {
+    pr_err("Cannot fid a process with pid %d\n", pid_nr);
+    goto no_pid;
+  }
+
+  struct task_struct *task = get_pid_task(pid, PIDTYPE_PID);
+
+  if (!task) {
+    pr_err("Cannot query task information for process with pid %d\n", pid_nr);
+    goto put_pid;
+  }
+
+  if (!pid_alive(task)) {
+    pr_err("Process with pid %d is no longer alive\n", pid_nr);
+    goto put_task_struct;
+  }
+
+  sample->utime = task->utime;
+  sample->stime = task->stime;
+  sample_okay = true;
+
+put_task_struct:
+  put_task_struct(task);
+
+put_pid:
+  put_pid(pid);
+
+no_pid:
+  return sample_okay;
+}
+
+static int render_task_sample(const struct task_monitor *monitor, struct task_sample *sample, char *buf, size_t buffer_len)
+{
+  return snprintf(buf, buffer_len, "pid %d usr %llu sys %llu", monitor->pid, sample->utime, sample->stime);
+}
+
 static int monitor_fn(void *arg)
 {
-  while (task_stats_update(&stats) == 0 && !kthread_should_stop()) {
+  while (get_sample(&task_monitor, &task_sample) && !kthread_should_stop()) {
     char buf[256];
 
-    task_stats_render(&stats, buf, sizeof(buf));
+    render_task_sample(&task_monitor, &task_sample, buf, sizeof(buf));
     pr_info("%s\n", buf);
     ssleep(1);
   }
@@ -104,6 +91,7 @@ static int monitor_fn(void *arg)
 
 static int start_monitor_fn(void)
 {
+  //TODO best use a mutex here
   if (monitor_fn_handle) {
     pr_err("monitor_fn is already running\n");
     return -1;
@@ -134,10 +122,10 @@ static int stop_monitor_fn(void)
 
 static ssize_t taskmonitor_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-  if (task_stats_update(&stats) == 0) {
+  if (get_sample(&task_monitor, &task_sample)) {
     char render_buf[256];
 
-    task_stats_render(&stats, render_buf, sizeof(render_buf));
+    render_task_sample(&task_monitor, &task_sample, render_buf, sizeof(render_buf));
     return sysfs_emit(buf, "%s\n", render_buf);
   }
 
@@ -159,10 +147,10 @@ static struct kobj_attribute monitor_attribute = __ATTR_RW(taskmonitor);
 static long unlocked_ioctl(struct file *file, unsigned int request_nr, unsigned long buf)
 {
   if (request_nr == TM_GET) {
-    if (task_stats_update(&stats) == 0) {
+    if (get_sample(&task_monitor, &task_sample)) {
       char render_buf[256];
 
-      task_stats_render(&stats, render_buf, sizeof(render_buf));
+      render_task_sample(&task_monitor, &task_sample, render_buf, sizeof(render_buf));
       return copy_to_user((void *) buf, render_buf, sizeof(render_buf));
     }
     return 0;
@@ -181,9 +169,9 @@ static long unlocked_ioctl(struct file *file, unsigned int request_nr, unsigned 
       return err;
     }
     if (arg < 0) {
-      return copy_to_user((void *) buf, &stats.pid, sizeof(stats.pid));
+      return copy_to_user((void *) buf, &task_monitor.pid, sizeof(task_monitor.pid));
     } else {
-      stats.pid = arg;
+      task_monitor.pid = arg;
       return 0;
     }
   } else {
@@ -194,12 +182,12 @@ static long unlocked_ioctl(struct file *file, unsigned int request_nr, unsigned 
 static int __init taskmonitor_init(void)
 {
   //setup stats
-  stats.pid = target;
-  int pid_err = task_stats_update(&stats);
+  task_monitor.pid = target;
+  bool sample_okay = get_sample(&task_monitor, &task_sample);
 
-  if (pid_err) {
-    pr_err("Encountered a problem with the provided pid\n");
-    return pid_err;
+  if (!sample_okay) {
+    pr_err("Failed to update the task sample\n");
+    return -1;
   }
 
   //monitor_fn kthread
